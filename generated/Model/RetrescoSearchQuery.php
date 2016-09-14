ff542f87768eef9734e8f0961a71755a448f780c
Adam Zielinski <maosmurf@gmail.com>
2016-09-14 12:56:58 +0200

    HER-196 RetrescoSearchQuery with all fields

--- generated/Model/RetrescoSearchQuery.php
+++ generated/Model/RetrescoSearchQuery.php
@@ -9,6 +9,66 @@ class RetrescoSearchQuery
      */
     protected $q;
     /**
+     * @var string
+     */
+    protected $searchFields;
+    /**
+     * @var mixed
+     */
+    protected $resultSize;
+    /**
+     * @var mixed
+     */
+    protected $resultFrom;
+    /**
+     * @var string
+     */
+    protected $sortField;
+    /**
+     * @var string
+     */
+    protected $authors;
+    /**
+     * @var string
+     */
+    protected $locations;
+    /**
+     * @var string
+     */
+    protected $persons;
+    /**
+     * @var string
+     */
+    protected $products;
+    /**
+     * @var string
+     */
+    protected $organisations;
+    /**
+     * @var string
+     */
+    protected $keywords;
+    /**
+     * @var string
+     */
+    protected $sources;
+    /**
+     * @var string
+     */
+    protected $docTypes;
+    /**
+     * @var mixed
+     */
+    protected $published;
+    /**
+     * @var mixed
+     */
+    protected $dateFrom;
+    /**
+     * @var mixed
+     */
+    protected $dateTo;
+    /**
      * @return string
      */
     public function getQ()
@@ -23,6 +83,261 @@ class RetrescoSearchQuery
     public function setQ($q = null)
     {
         $this->q = $q;
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getSearchFields()
+    {
+        return $this->searchFields;
+    }
+    /**
+     * @param string $searchFields
+     *
+     * @return self
+     */
+    public function setSearchFields($searchFields = null)
+    {
+        $this->searchFields = $searchFields;
+        return $this;
+    }
+    /**
+     * @return mixed
+     */
+    public function getResultSize()
+    {
+        return $this->resultSize;
+    }
+    /**
+     * @param mixed $resultSize
+     *
+     * @return self
+     */
+    public function setResultSize($resultSize = null)
+    {
+        $this->resultSize = $resultSize;
+        return $this;
+    }
+    /**
+     * @return mixed
+     */
+    public function getResultFrom()
+    {
+        return $this->resultFrom;
+    }
+    /**
+     * @param mixed $resultFrom
+     *
+     * @return self
+     */
+    public function setResultFrom($resultFrom = null)
+    {
+        $this->resultFrom = $resultFrom;
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getSortField()
+    {
+        return $this->sortField;
+    }
+    /**
+     * @param string $sortField
+     *
+     * @return self
+     */
+    public function setSortField($sortField = null)
+    {
+        $this->sortField = $sortField;
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getAuthors()
+    {
+        return $this->authors;
+    }
+    /**
+     * @param string $authors
+     *
+     * @return self
+     */
+    public function setAuthors($authors = null)
+    {
+        $this->authors = $authors;
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getLocations()
+    {
+        return $this->locations;
+    }
+    /**
+     * @param string $locations
+     *
+     * @return self
+     */
+    public function setLocations($locations = null)
+    {
+        $this->locations = $locations;
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getPersons()
+    {
+        return $this->persons;
+    }
+    /**
+     * @param string $persons
+     *
+     * @return self
+     */
+    public function setPersons($persons = null)
+    {
+        $this->persons = $persons;
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+    /**
+     * @param string $products
+     *
+     * @return self
+     */
+    public function setProducts($products = null)
+    {
+        $this->products = $products;
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getOrganisations()
+    {
+        return $this->organisations;
+    }
+    /**
+     * @param string $organisations
+     *
+     * @return self
+     */
+    public function setOrganisations($organisations = null)
+    {
+        $this->organisations = $organisations;
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getKeywords()
+    {
+        return $this->keywords;
+    }
+    /**
+     * @param string $keywords
+     *
+     * @return self
+     */
+    public function setKeywords($keywords = null)
+    {
+        $this->keywords = $keywords;
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getSources()
+    {
+        return $this->sources;
+    }
+    /**
+     * @param string $sources
+     *
+     * @return self
+     */
+    public function setSources($sources = null)
+    {
+        $this->sources = $sources;
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getDocTypes()
+    {
+        return $this->docTypes;
+    }
+    /**
+     * @param string $docTypes
+     *
+     * @return self
+     */
+    public function setDocTypes($docTypes = null)
+    {
+        $this->docTypes = $docTypes;
+        return $this;
+    }
+    /**
+     * @return mixed
+     */
+    public function getPublished()
+    {
+        return $this->published;
+    }
+    /**
+     * @param mixed $published
+     *
+     * @return self
+     */
+    public function setPublished($published = null)
+    {
+        $this->published = $published;
+        return $this;
+    }
+    /**
+     * @return mixed
+     */
+    public function getDateFrom()
+    {
+        return $this->dateFrom;
+    }
+    /**
+     * @param mixed $dateFrom
+     *
+     * @return self
+     */
+    public function setDateFrom($dateFrom = null)
+    {
+        $this->dateFrom = $dateFrom;
+        return $this;
+    }
+    /**
+     * @return mixed
+     */
+    public function getDateTo()
+    {
+        return $this->dateTo;
+    }
+    /**
+     * @param mixed $dateTo
+     *
+     * @return self
+     */
+    public function setDateTo($dateTo = null)
+    {
+        $this->dateTo = $dateTo;
         return $this;
     }
 }
--- generated/Normalizer/RetrescoSearchQueryNormalizer.php
+++ generated/Normalizer/RetrescoSearchQueryNormalizer.php
@@ -37,6 +37,51 @@ class RetrescoSearchQueryNormalizer extends SerializerAwareNormalizer implements
         if (property_exists($data, 'q')) {
             $object->setQ($data->{'q'});
         }
+        if (property_exists($data, 'search_fields')) {
+            $object->setSearchFields($data->{'search_fields'});
+        }
+        if (property_exists($data, 'result_size')) {
+            $object->setResultSize($data->{'result_size'});
+        }
+        if (property_exists($data, 'result_from')) {
+            $object->setResultFrom($data->{'result_from'});
+        }
+        if (property_exists($data, 'sort_field')) {
+            $object->setSortField($data->{'sort_field'});
+        }
+        if (property_exists($data, 'authors')) {
+            $object->setAuthors($data->{'authors'});
+        }
+        if (property_exists($data, 'locations')) {
+            $object->setLocations($data->{'locations'});
+        }
+        if (property_exists($data, 'persons')) {
+            $object->setPersons($data->{'persons'});
+        }
+        if (property_exists($data, 'products')) {
+            $object->setProducts($data->{'products'});
+        }
+        if (property_exists($data, 'organisations')) {
+            $object->setOrganisations($data->{'organisations'});
+        }
+        if (property_exists($data, 'keywords')) {
+            $object->setKeywords($data->{'keywords'});
+        }
+        if (property_exists($data, 'sources')) {
+            $object->setSources($data->{'sources'});
+        }
+        if (property_exists($data, 'doc_types')) {
+            $object->setDocTypes($data->{'doc_types'});
+        }
+        if (property_exists($data, 'published')) {
+            $object->setPublished($data->{'published'});
+        }
+        if (property_exists($data, 'date_from')) {
+            $object->setDateFrom($data->{'date_from'});
+        }
+        if (property_exists($data, 'date_to')) {
+            $object->setDateTo($data->{'date_to'});
+        }
         return $object;
     }
     public function normalize($object, $format = null, array $context = array())
@@ -44,6 +89,51 @@ class RetrescoSearchQueryNormalizer extends SerializerAwareNormalizer implements
         $data = new \stdClass();
         if (null !== $object->getQ()) {
             $data->{'q'} = $object->getQ();
+        }
+        if (null !== $object->getSearchFields()) {
+            $data->{'search_fields'} = $object->getSearchFields();
+        }
+        if (null !== $object->getResultSize()) {
+            $data->{'result_size'} = $object->getResultSize();
+        }
+        if (null !== $object->getResultFrom()) {
+            $data->{'result_from'} = $object->getResultFrom();
+        }
+        if (null !== $object->getSortField()) {
+            $data->{'sort_field'} = $object->getSortField();
+        }
+        if (null !== $object->getAuthors()) {
+            $data->{'authors'} = $object->getAuthors();
+        }
+        if (null !== $object->getLocations()) {
+            $data->{'locations'} = $object->getLocations();
+        }
+        if (null !== $object->getPersons()) {
+            $data->{'persons'} = $object->getPersons();
+        }
+        if (null !== $object->getProducts()) {
+            $data->{'products'} = $object->getProducts();
+        }
+        if (null !== $object->getOrganisations()) {
+            $data->{'organisations'} = $object->getOrganisations();
+        }
+        if (null !== $object->getKeywords()) {
+            $data->{'keywords'} = $object->getKeywords();
+        }
+        if (null !== $object->getSources()) {
+            $data->{'sources'} = $object->getSources();
+        }
+        if (null !== $object->getDocTypes()) {
+            $data->{'doc_types'} = $object->getDocTypes();
+        }
+        if (null !== $object->getPublished()) {
+            $data->{'published'} = $object->getPublished();
+        }
+        if (null !== $object->getDateFrom()) {
+            $data->{'date_from'} = $object->getDateFrom();
+        }
+        if (null !== $object->getDateTo()) {
+            $data->{'date_to'} = $object->getDateTo();
         }
         return $data;
     }
