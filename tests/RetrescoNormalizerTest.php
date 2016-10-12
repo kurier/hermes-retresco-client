a056d17313ef635ad6723f5445db1efdcc7ec3a0
Adam Zielinski <maosmurf@gmail.com>
2016-10-12 20:48:40 +0200

    HER-97 HER-122 HER-194 fix RetrescoNormalizerTest by adding RawEncoder

--- tests/RetrescoNormalizerTest.php
+++ tests/RetrescoNormalizerTest.php
@@ -8,7 +8,14 @@
 namespace telekurier\Retresco\Tests;
 
 use FR3D\SwaggerAssertions\PhpUnit\Psr7AssertsTrait;
+use Joli\Jane\Encoder\RawEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Serializer;
+use telekurier\RetrescoClient\Model\Location;
+use telekurier\RetrescoClient\Model\Pin;
 use telekurier\RetrescoClient\Model\RetrescoDocument;
+use telekurier\RetrescoClient\Normalizer\LocationNormalizer;
+use telekurier\RetrescoClient\Normalizer\PinNormalizer;
 use telekurier\RetrescoClient\Normalizer\RetrescoDocumentNormalizer;
 use telekurier\RetrescoClient\RetrescoClient;
 
@@ -27,6 +34,11 @@ class RetrescoNormalizerTest extends \PHPUnit_Framework_TestCase {
   protected $normalizer;
 
   /**
+   * @var Serializer
+   */
+  protected $serializer;
+
+  /**
    * @var RetrescoDocument
    */
   protected $document;
@@ -35,7 +47,20 @@ class RetrescoNormalizerTest extends \PHPUnit_Framework_TestCase {
    * {@inheritdoc}
    */
   protected function setUp() {
-    $this->normalizer = new RetrescoDocumentNormalizer();
+
+    $retrescoDocumentNormalizer = new RetrescoDocumentNormalizer();
+
+    $encoders = [new JsonEncoder(), new RawEncoder()];
+    $normalizers = [
+      $retrescoDocumentNormalizer,
+      new PinNormalizer(),
+      new LocationNormalizer(),
+    ];
+
+    $this->serializer = new Serializer($normalizers, $encoders);
+
+    $this->normalizer = $retrescoDocumentNormalizer;
+
     $this->document = new RetrescoDocument();
     parent::setUp();
   }
@@ -57,5 +82,25 @@ class RetrescoNormalizerTest extends \PHPUnit_Framework_TestCase {
     $this->document->setPayload($payload);
     $obj = $this->normalizer->normalize($this->document);
     $this->assertEquals($payload, $obj->payload);
+  }
+
+  /**
+   * Tests normalization of PIN
+   */
+  public function testNormalizePin() {
+    $location = new Location();
+    $location->setLat(-1);
+    $location->setLon(1);
+    $pin = new Pin();
+    $pin->setLocation($location);
+    $this->document->setPin($pin);
+
+    $normalizedPin = new \stdClass();
+    $normalizedPin->location = new \stdClass();
+    $normalizedPin->location->lat = -1;
+    $normalizedPin->location->lon = 1;
+
+    $obj = $this->normalizer->normalize($this->document);
+    $this->assertEquals($normalizedPin, $obj->pin);
   }
 }
